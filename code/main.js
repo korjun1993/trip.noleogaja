6d64d3f00fb43b3dab030d16ebe367fea58aa452
tjantjd26 <tjantjd26@naver.com>
2019-10-16 12:14:37 +0900

    [ 기능 : 날짜로 행사 검색 기능 추가 ]
    날짜 및 기간을 통한 행사 검색 기능 추가(빅스비 time 라이브러리를 이용)
    ex - '다음주', '내년', '7월 18일' 등
    
    관련 : #S1P4151008-9, #S1P4151008-24

--- code/main.js
+++ code/main.js
@@ -1,40 +1,87 @@
 const data = require("StoreData.js")
 var textLib = require('textLib')
 var console = require('console')
+var dates = require("dates")
 
 module.exports = {
   "findFestivals": findFestivals,
 }
 
-function findFestivals(location, date) {
+function findFestivals(location, dateTimeExpression) {
   console.log(data())
-  return filterFestivals(data(),location, date)
+  return filterFestivals(data(),location, dateTimeExpression)
   
 }
 
-function filterFestivals(candidates, location, date) {
+function filterFestivals(candidates, location, dateTimeExpression) {
   if (location) {
     candidates = candidates.filter(function(candidate){
       return candidate.location.toLowerCase() == location.toLowerCase()
     })
   }
 
-  if (date) {
-    candidates = candidates.filter(function(candidate){
-      console.log(date.substring(0,1))
-      if(Number(candidate.beginDate.substring(0,1)) > Number(candidate.endDate.substring(0,1)))
-      {
-        return (Number(candidate.beginDate.substring(0,1))) <= Number(date.substring(0,1)) && Number(date.substring(0,1))<= (Number(candidate.endDate.substring(0,1))+12)
-      }
-      else
-      {
-        return (Number(candidate.beginDate.substring(0,1))) <= Number(date.substring(0,1)) && Number(date.substring(0,1))<= (Number(candidate.endDate.substring(0,1)))
-      }
-      
-    })
+  if (dateTimeExpression) { //string -> date
+    if (dateTimeExpression.date) { //날짜로 들어온 경우
+      console.log(dateTimeExpression.date)
+      var when = new Date(dates.ZonedDateTime.fromDate(dateTimeExpression.date).toIsoString());
+       candidates = candidates.filter(function(candidate){
+
+        var y = candidate.beginDate.substr(0,4);
+        var m = candidate.beginDate.substr(4,2);
+        var d = candidate.beginDate.substr(6,2); 
+        var startDate = new Date(y, m - 1, d); //해당 행사의 시작날짜
+
+        y = candidate.endDate.substr(0,4);
+        m = candidate.endDate.substr(4,2);
+        d = candidate.endDate.substr(6,2);
+        var endDate = new Date(y, m - 1, d); //해당 행사의 종료날짜
+        
+        if((startDate <= when) && (endDate >= when)){
+          console.log(startDate);
+          console.log(when);
+          console.log(endDate);
+          console.log("@@@@@@@@@@@@@@@@@")
+        }
+        return (startDate <= when) && (endDate >= when)
+      })
+    }
+    else if (dateTimeExpression.dateInterval) { //기간으로 들어온 경우
+      console.log(dateTimeExpression.dateInterval);
+      whenStart = new Date(dates.ZonedDateTime.of(
+        dateTimeExpression.dateInterval.start.year,
+        dateTimeExpression.dateInterval.start.month,
+        dateTimeExpression.dateInterval.start.day).toIsoString());
+        
+      whenEnd = new Date(dates.ZonedDateTime.of(
+        dateTimeExpression.dateInterval.end.year,
+        dateTimeExpression.dateInterval.end.month,
+        dateTimeExpression.dateInterval.end.day).toIsoString());
+
+        candidates = candidates.filter(function(candidate){
+          var y = candidate.beginDate.substr(0,4);
+          var m = candidate.beginDate.substr(4,2);
+          var d = candidate.beginDate.substr(6,2); 
+          var startDate = new Date(y, m - 1, d); //해당 행사의 시작날짜
+
+          y = candidate.endDate.substr(0,4);
+          m = candidate.endDate.substr(4,2);
+          d = candidate.endDate.substr(6,2);
+          var endDate = new Date(y, m - 1, d); //해당 행사의 종료날짜
+
+          if(((startDate >= whenStart) && (startDate <= whenEnd)) || ((endDate >= whenStart) && (endDate <= whenEnd))){
+            console.log(startDate);
+            console.log(whenStart);
+            console.log(whenEnd);
+            console.log(endDate);
+            console.log("@@@@@@@@@@@@@@@@@")
+          }
+          return ((startDate >= whenStart) && (startDate <= whenEnd)) || ((endDate >= whenStart) && (endDate <= whenEnd))
+        })
+    }
+    
   }
-
-
+  
   return candidates;
+
 }
 
