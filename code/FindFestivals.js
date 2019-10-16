ad57bb4cb7065642e12603d81510520dfb08fc11
Baekyu Seo <ponta12@naver.com>
2019-10-16 19:08:45 +0900

    [ 기능 : 축제 찾는 기능 변경 ]
    축제를 찾는 기능을 외부 api를 통해서 받아올 수 있도록 수정했습니다. 또한,
    받아온 지역을 이용해서 지역 코드를 알아내는 메서드를 추가했음
    
    관련 : S1P4151008-16

--- code/FindFestivals.js
+++ code/FindFestivals.js
@@ -1,20 +1,40 @@
-//   module.exports.function = function findFestivals (location) {
-//   const fakeData = require("StoreData.js");
-//   const console = require("console");
-  
-//   var fesinfo = [];
-//   for(let i  =0; i<fakeData.length;i++)
-//   {
-//     if(fakeData[i].location == String(location))
-//     {
-//       fesinfo.push(fakeData[i]);
-//     }
-//   }
-  
-//   return fesinfo;
-// }
+var baseURL = 'http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchFestival';
+var key = 'durJHB4Hx8wuHRX6IU2cY1TW%2BbXLOTxyoLSYFV4FQmx4MzmDWvrKzFzwPtUqD3Bjte974mth8StXqjseFlCR7A%3D%3D';
+var http = require('http');
+var console = require('console')
+
 const main = require("main.js")
+const getAreaCode = require('GetAreaCode.js')
+var options = {
+  format: 'json',
+  headers: {
+    'accept': 'application/json'
+  },
+};
 
 module.exports.function = function findFestivals (location, date) {
+
+  let pageNo = 1;
+  let queryParams = '?' + encodeURIComponent('ServiceKey') + '=' + key;
+  queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('20');
+  queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(pageNo);
+  queryParams += '&' + encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC');
+  queryParams += '&' + encodeURIComponent('MobileApp') + '=' + encodeURIComponent('AppTest');
+  queryParams += '&' + encodeURIComponent('arrange') + '=' + encodeURIComponent('P');
+  
+  if(location != null) {
+    let code = getAreaCode.getAreaCode('전라남도-나주시');
+    queryParams += '&' + encodeURIComponent('areaCode') + '=' + encodeURIComponent(code.mainLoc);
+    if(code.subLoc != 0) {
+      queryParams += '&' + encodeURIComponent('sigunguCode') + '=' + encodeURIComponent(code.subLoc);
+    }
+  }
+
+  queryParams += '&' + encodeURIComponent('eventStartDate') + '=' + encodeURIComponent('20191016');
+
+  let response = http.getUrl(baseURL + queryParams, options);
+
+  console.log(response)
+  
   return main.findFestivals(location, date);
 }
