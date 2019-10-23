9ab8fe78c3c5fc239d4a6d995358a67c6ce290ab
Baekyu Seo <ponta12@naver.com>
2019-10-23 18:09:18 +0900

    [ 기타 : merge 충돌 해결 ]

---
+++ code/changePage.js
@@ -0,0 +1,24 @@
+var console = require('console');
+var service = require('./service')
+
+module.exports.function = function changePage (festivalList, location, date, dateInterval, pageChange) {
+  
+  var numOfRows = 20;
+  var totalCount = festivalList.totalCount;
+  var pageNo = festivalList.pageNo;
+  var remain = totalCount % numOfRows;
+  var devide = totalCount / numOfRows;
+  var pageLength = remain > 0 ? devide + 1 : devide;
+
+  if(pageChange == '다음' && pageNo < pageLength){
+    pageNo++;
+  }
+  
+  else if(pageChange == '이전' && pageNo > 1){
+    pageNo--;
+  }
+  
+  console.log(service);
+
+  return service.findFestivals(location, date, dateInterval, pageNo);
+}
