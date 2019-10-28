99a3b4c7c97e884d8b6fe60d33d55c2a6ce8205e
tjantjd26 <tjantjd26@naver.com>
2019-10-28 14:02:39 +0900

    [ 기능 : 상세 페이지에서 추가 기능 버튼 추가 ]
    상세페이지에서 전화연결, 지도, 홈페이지로의 이동 기능 및 발화 추가

--- code/lib/openBrowser.js
+++ code/lib/openBrowser.js
@@ -1,16 +1,19 @@
-module.exports.function = function openBrowser (homePage, tel, point, showDetail) {
+module.exports.function = function openBrowser (showDetail, showDetailType) {
   const console = require('console');
   const config = require('config');
+  const fail = require('fail');
   let result = '';
-
-  if(tel != undefined) {
-    result = "tel:" + tel;
-  } else if(point != undefined){
-    result = 'https://m.map.naver.com/map.nhn?lat=' + point.latitude + '&lng=' + point.longitude + '&dlevel=11&mapMode=&pinTitle=&boundary=&traffic='
-  } else if(showDetail != undefined) {
-    result = config.get('youtube.url') + showDetail.title;
+  if(showDetail == undefined){
+    throw fail.checkedError('There is no festival data', 'NotFoundFestival', null); 
+  }
+  if(showDetailType == 1) {
+    result = "tel:" + showDetail.tel;
+  } else if(showDetailType == 2){
+    result = 'https://m.map.naver.com/map.nhn?lat=' + showDetail.point.latitude + '&lng=' + showDetail.point.longitude + '&dlevel=11&mapMode=&pinTitle=&boundary=&traffic='
+  } else if(showDetailType == 3) {
+    result = showDetail.homePage;
   } else {
-    result = homePage;
+    result = config.get('youtube.url') + showDetail.title;
   } 
 
   return result;
